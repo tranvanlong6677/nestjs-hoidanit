9d2c54731c88e9345db86af88aec4e3074bde1c4
Long Tran <tranvanlong6677@gmail.com>
2024-02-03 15:19:15 +0700

    mvc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 @Module({
-  imports: [UsersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
