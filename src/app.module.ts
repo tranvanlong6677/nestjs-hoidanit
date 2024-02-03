890e27a862b502cc6192262c929be003988d8a35
Long Tran <tranvanlong6677@gmail.com>
2024-02-04 00:58:06 +0700

    create schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [ConfigModule.forRoot(
-    { isGlobal: true }
-  ), MongooseModule.forRootAsync({
-    imports: [ConfigModule],
-    useFactory: async (configService: ConfigService) => ({
-      uri: configService.get<string>('MONGO_URI'),
+  imports: [
+    ConfigModule.forRoot(
+      { isGlobal: true }
+    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
     }),
-    inject: [ConfigService],
-  })],
+
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
